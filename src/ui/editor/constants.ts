c2e71f85cfe26c1b534c003480c20c1c3a02bd6b
isqua <isqua@isqua.ru>
2025-07-17 20:57:07 +0200

    feat: enhance the placeholder

--- src/ui/editor/constants.ts
+++ src/ui/editor/constants.ts
@@ -1,3 +1,3 @@
-export const PLACEHOLDER_TEXT = 'Press Ctrl+Space to show suggest';
+export const PLACEHOLDER_TEXT = 'Try to type something, like `author = amanda`';
 export const HISTORY_GROUP_DELAY_IN_MS = 200;
 export const ONCHANGE_DEBOUNCE_TIMEOUT_IN_MS = 100;
